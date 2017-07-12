deaa6fb456595e7166da8a5a080465e49f7ceecd
mtclark0518 <mtclark0518@gmail.com>
2017-07-12 13:04:24 -0600

    adds structure and some UX functionality

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 $(function(){
 
+//INSTRUCTIONS POPUP FUNCTIONALITY
+
+	$("#triggerInstructions").click(function(){
+		$("#instructions").toggleClass("hidden");
+	});
+
+	$(".close").click(function(){
+		$(this).parent().addClass("hidden");
+	});
+
+	$("#triggerPlay").click(function(){
+		$("#game_cover").addClass("hidden");
+	});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
